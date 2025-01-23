666bad1d2b56186eea75b5d89d6fa9c85c848228
itskrns <itskrns@gmail.com>
2025-01-24 00:07:58 +0530

    Update Errors

--- src/pages/Cabins.jsx
+++ src/pages/Cabins.jsx
@@ -1,6 +1,6 @@
 import Row from "../ui/Row";
 import Heading from "../ui/Heading";
-import AddCabins from "../features/cabins/addCabins";
+import AddCabins from "../features/cabins/AddCabins";
 import CabinTable from "../features/cabins/CabinTable";
 import CabinOperations from "../features/cabins/CabinOperations";
 
